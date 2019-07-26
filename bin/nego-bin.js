384a1f6195998785ae4c19ca42f1aad4a4bb9481
CuveeCui <18280133086@163.com>
2019-07-26 15:21:58 +0800

    add nodemon to hot restart

--- bin/nego-bin.js
+++ bin/nego-bin.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 
-const NegoBin = require('../lib');
 
-new NegoBin().start();
+const nodemon = require('nodemon');
+const { resolve } = require('path');
+nodemon({
+  script: `${resolve(__dirname, '../lib/index.js')}`,
+  ext: 'js json'
+});
+
+nodemon.on('start', function () {
+  console.log('App has started');
+}).on('quit', function () {
+  console.log('App has quit');
+  process.exit();
+}).on('restart', function (files) {
+  console.log('App restarted due to: ', files);
+});
+
+
